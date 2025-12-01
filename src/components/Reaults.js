57a5078c13d9c4d07dd535e2cb760d61b51f571b
dahye0937 <dahye0937@gmail.com>
2025-12-01 09:21:07 +0900

    init: create project

--- src/components/Reaults.js
+++ src/components/Reaults.js
@@ -2,7 +2,7 @@ const Reaults = ({onReStart,score}) => {
   return (
     <div id="reault">
         <h2>퀴즈 종료</h2>
-        <p>최종점수 : {score}</p>
+        <p>오늘의 점수 : {score}</p>
         <button onClick={onReStart}>다시시작</button>
     </div>
   )
